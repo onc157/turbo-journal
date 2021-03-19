764a5e0dbb9397104fb045723bfce706fa926581
onc157 <onc157@yandex.ru>
2021-03-20 00:47:02 +0300

    init: Created app and added basic markup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.scss';
+import Navbar from '../Navbar/Navbar';
+import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.scss';
+
+function Footer() {
+  return <>Footer</>;
+}
+
+export default Footer;
